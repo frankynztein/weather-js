fd0cec38d07c27d79d51325e9e078f2f601264c6
frankynztein <arellanfranciree@gmail.com>
2024-11-24 19:21:38 +0100

    weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,22 +1,17 @@
 export default async function handler(req, res) {
   const { endPoint, city } = req.query;
+
   if (!city || !endPoint) {
       return res.status(400).json({ error: "Both 'city' and 'endPoint' parameters are required." });
   }
 
   const apiKey = process.env.OPENWEATHER_API_KEY;
+  if (!apiKey) {
+      return res.status(500).json({ error: "API key is missing. Please set it in the environment variables." });
+  }
+
   const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
 
   try {
       const response = await fetch(apiUrl);
-      const data = await response.json();
-
-      if (data.cod !== 200) {
-          return res.status(data.cod).json({ error: data.message });
-      }
-
-      res.status(200).json(data);
-  } catch (error) {
-      res.status(500).json({ error: "Internal Server Error" });
-  }
-}
+      const data = await response
